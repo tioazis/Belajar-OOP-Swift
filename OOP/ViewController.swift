2117d24bd65520eb9706fa0af543ae91b4d8edf0
Azis Jabbar Susetio <tioazis@gmail.com>
2019-05-15 13:21:15 +0700

    Initial Commit

---
+++ OOP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OOP
+//
+//  Created by Azis Jabbar Susetio on 15/05/19.
+//  Copyright © 2019 Azis Susetio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
